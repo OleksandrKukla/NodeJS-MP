2fe0ccc43c4314c4b242965c20581105a794492f
oleksandrkukla <olexandrkukla@gmail.com>
2019-11-27 01:00:57 +0200

    task 1.1

---
+++ task1.js
@@ -0,0 +1,20 @@
+const ReadLine = require('readline');
+
+process.stdin.setEncoding('utf8');
+
+const readLine = ReadLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readLine.setPrompt('Input: ');
+
+readLine.on('line', (inputString) => {
+    readLine.write('Output: ');
+    readLine.write([...inputString].reverse().join(''));
+
+    readLine.clearLine();
+    readLine.prompt();
+});
+
+readLine.prompt();
